c92c371f38f22c93ca60ea2994fae9dbbba96649
SerG <serg.arnisha@gmail.com>
2022-12-11 22:53:03 +0100

    props

--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -1,14 +1,12 @@
 import EmployeesListItem from "../employees-list-item/employees-list-item";
 import "./employees-list.css";
 
-const EmployeesList = () => {
-  return (
-    <ul className="app-list list-group">
-      <EmployeesListItem />
-      <EmployeesListItem />
-      <EmployeesListItem />
-    </ul>
-  );
+const EmployeesList = ({ dataServ }) => {
+  const elements = dataServ.map((item) => {
+    return <EmployeesListItem {...item} />; // name={item.name} salary={item.salary}
+  });
+
+  return <ul className="app-list list-group">{elements}</ul>;
 };
 
 export default EmployeesList;
