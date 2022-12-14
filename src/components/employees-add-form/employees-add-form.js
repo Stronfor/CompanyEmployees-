372e7f553157e85bd0491e8fac3b585c04e1b4f0
SerG <serg.arnisha@gmail.com>
2022-12-14 16:11:27 +0100

    fix form

--- src/components/employees-add-form/employees-add-form.js
+++ src/components/employees-add-form/employees-add-form.js
@@ -1,28 +1,55 @@
+import { Component } from "react";
+
 import "./employees-add-form.css";
 
-const EmployeesAddForm = () => {
-  return (
-    <div className="app-add-form">
-      <h3>Aggiungire uno nuovo Dipendente</h3>
-      <form className="add-form d-flex">
-        <input
-          type="text"
-          className="form-control new-post-label"
-          placeholder="Come si chiama?"
-        />
+class EmployeesAddForm extends Component {
+  constructor(props) {
+    super(props);
 
-        <input
-          type="number"
-          className="form-control new-post-label"
-          placeholder="Stipendio in Euro?"
-        />
+    this.state = {
+      name: "",
+      salary: "",
+    };
+  }
 
-        <button type="submit" className="btn btn-outline-light">
-          Aggungire
-        </button>
-      </form>
-    </div>
-  );
-};
+  onValueChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  render() {
+    const { name, salary } = this.state;
+
+    return (
+      <div className="app-add-form">
+        <h3>Aggiungire uno nuovo Dipendente</h3>
+        <form className="add-form d-flex">
+          <input
+            type="text"
+            className="form-control new-post-label"
+            placeholder="Come si chiama?"
+            name="name"
+            value={name} /* записывает актуальное значение для оптимизации */
+            onChange={this.onValueChange}
+          />
+
+          <input
+            type="number"
+            className="form-control new-post-label"
+            placeholder="Stipendio in Euro?"
+            name="salary"
+            value={salary} /* записывает актуальное значение для оптимизации */
+            onChange={this.onValueChange}
+          />
+
+          <button type="submit" className="btn btn-outline-light">
+            Aggungire
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default EmployeesAddForm;
